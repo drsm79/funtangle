2fb26e88566412eb81720aee428c6f02aae4c042
Simon Metson <simon+github@cloudant.com>
2015-05-23 17:37:23 +0100

    Add sequencer

--- launchpad.js
+++ launchpad.js
@@ -2,26 +2,26 @@
 // https://github.com/justinlatimer/node-midi
 var _ = require('underscore');
 var midi = require('midi');
-var output = new midi.output();
+var masteroutput = new midi.output();
 
 var controllers = {};
 console.log('Pick your launchpad');
-_(output.getPortCount()).times(function(n){
-    controllers[output.getPortName(n)] = n;
-    console.log(output.getPortName(n), ': ', n);
+_(masteroutput.getPortCount()).times(function(n){
+    controllers[masteroutput.getPortName(n)] = n;
+    console.log(masteroutput.getPortName(n), ': ', n);
 });
 
+var outputs = {};
 var prompt = require('prompt');
 
 prompt.get(
     _.first([
         'launchpad',
-        {name: 'synth1', required: false},
-        {name: 'synth2', required: false},
-        {name: 'synth3', required: false},
-        {name: 'synth4', required: false},
-        {name: 'synth5', required: false}
-    ], output.getPortCount()),
+        {name: 'synth1', required: false, default: -1, type: 'number'},
+        {name: 'synth2', required: false, default: -1, type: 'number'},
+        {name: 'synth3', required: false, default: -1, type: 'number'},
+        {name: 'synth4', required: false, default: -1, type: 'number'}
+    ], masteroutput.getPortCount()),
 function (err, result) {
     // https://github.com/sydlawrence/node-midi-launchpad
     var launchpad = require('midi-launchpad').connect(parseInt(result.launchpad));
@@ -37,6 +37,7 @@ function (err, result) {
 
     function initcontrols() {
         launchpad.clear();
+        notesOff();
         play.light(launchpad.colors.green.high);
         stop.light(launchpad.colors.red.medium);
         shift.light(launchpad.colors.yellow.medium);
@@ -55,6 +56,7 @@ function (err, result) {
             // Stop the clock without reseting the tick
             clock.stop();
             play.light(launchpad.colors.green.high);
+            notesOff();
         } else {
             clock.start();
             console.log(tempo);
@@ -68,22 +70,25 @@ function (err, result) {
         clock.stop();
         _(8).times(function(n){darktick(launchpad.getButton(n, 8)); });
         ticks = cycle(_.range(8));
+        notesOff();
         play.light(launchpad.colors.green.high);
         stop.light(launchpad.colors.red.medium);
     });
 
-    var bankcolours = cycle([
+    var bankcolours = [
         launchpad.colors.green.high,
         launchpad.colors.yellow.high,
         launchpad.colors.orange.high,
         launchpad.colors.red.high,
-        0]);
+        0];
+
+    var bankcycle = cycle(bankcolours);
 
     bank.on("press", function(button){
         if (shifted){
             initcontrols();
         } else {
-            bank.light(bankcolours.next());
+            bank.light(bankcycle.next());
         }
     });
 
@@ -138,6 +143,17 @@ function (err, result) {
         button.light((state == 0 ? launchpad.colors.yellow.medium : 0));
     }
 
+    function addNote(button){
+        var current = button.getState();
+        if (current == bank.getState()){
+            button.light(0);
+            // TODO: remove from list
+        } else {
+            button.light(bank.getState());
+            outputs[bank.getState()].notes[button.x].push(button.y);
+        }
+    }
+
     launchpad.on("press", function(button) {
         if (button.y == 8){
             if (!shifted){
@@ -146,16 +162,32 @@ function (err, result) {
                 glide(button);
             }
         } else if (button.x != 8) {
-            button.light(bank.getState());
+            addNote(button);
         }
     });
 
-    output.openPort(parseInt(result.synth1), "funtangle");
+    _(masteroutput.getPortCount() - 1).times(function(n){
+        var name = 'synth' + (n + 1);
+        var synth = parseInt(result['synth' + (n + 1)]);
+        if (synth >= 0){
+            outputs[bankcolours[n]] = {
+                output: new midi.output(),
+                notes: [[], [], [], [], [], [], [], []]
+            };
+            outputs[bankcolours[n]].output.openPort(synth, "funtangle");
+        }
+    });
 
     clock.on('position', function(position){
       var microPosition = position % 24;
       if (microPosition === 0){
         t = ticks.next();
+        donotesfor(launchpad, t, 144);
+        if (t > 0){
+            donotesfor(launchpad, t-1, 128);
+        } else {
+            donotesfor(launchpad, 7, 128);
+        }
         lighttick(launchpad.getButton(t, 8));
         if (t > 0){
             darktick(launchpad.getButton(t - 1, 8));
@@ -182,16 +214,30 @@ var shifted = false;
 var notes = [];
 
 var scale = teoria.note("g4").scale('dorian').notes();
-scale.push(scale[0].interval('P8'))
-
-function playnotesfor(beat){
-    ticknotes = notes[beat];
-    for (var i = ticknotes.length - 1; i >= 0; i--) {
-        output.sendMessage([144, scale.get(ticknotes[i]).midi(), 90]);
-        output.sendMessage([128, scale.get(ticknotes[i]).midi(), 90]);
-    };
+scale.push(scale[0].interval('P8'));
+// Keeping 'natural' numbering of keypads means reversing the scale so highest
+// note is highest pad
+scale.reverse();
+// Midi messages:
+// 128 = stop note, channel 1 (increase by 1 for each channel)
+// 144 = play note, channel 1 (increase by 1 for each channel)
+function donotesfor(launchpad, beat, message){
+    _.each(outputs, function(value, key, list){
+        var ticknotes = value.notes[beat];
+        var accented = launchpad.getButton(beat, 8).getState() == launchpad.colors.green.medium;
+        var velocity = accented ? 120 : 90;
+        for (var i = ticknotes.length - 1; i >= 0; i--) {
+            var note = scale[ticknotes[i]].midi();
+            value.output.sendMessage([message, note, velocity]);
+        };
+    });
 }
 
+function notesOff(){
+    _.each(outputs, function(value, key, list){
+        value.output.sendMessage([176, 120, 0])
+    });
+}
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
 function cycle(array){
     var nextIndex = 0;

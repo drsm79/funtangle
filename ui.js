04b1e2baad1813e313b44d3c776a7c50572cfd4e
Simon Metson <simon@metsonet.co.uk>
2015-06-23 00:25:48 +0100

    clear out some cruft

--- ui.js
+++ ui.js
@@ -46,25 +46,3 @@ exports.parseArgs = function(){
   .showHelpOnFail()
   .argv;
 }
-
-
-
-// console.log('Pick your launchpad');
-// _(masteroutput.getPortCount()).times(function(n){
-//     controllers[masteroutput.getPortName(n)] = n;
-//     console.log('out', masteroutput.getPortName(n), ': ', n);
-// });
-
-// // Configure the launchpad and set up the banks
-// prompt.get(
-//   _.first([
-//     'launchpad',
-//     {name: 'synth1', required: false, default: -1, type: 'number'},
-//     {name: 'synth2', required: false, default: -1, type: 'number'},
-//     {name: 'synth3', required: false, default: -1, type: 'number'},
-//     {name: 'synth4', required: false, default: -1, type: 'number'}
-//   ], masteroutput.getPortCount()),
-//   function (err, result) {
-
-// });
-
